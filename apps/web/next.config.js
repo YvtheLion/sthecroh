5c250a4572b19e659b7eee03293ed98f98adea14
YvtheLion <37544171+YvtheLion@users.noreply.github.com>
2026-08-18 13:14:33 -0400

    Sauvegarde automatique DB + redirection www

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,5 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.sthecroh.com' }],
+        destination: 'https://sthecroh.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 module.exports = nextConfig;
