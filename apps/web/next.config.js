88de2278211b3eb0f58af8c75f71000b87b65405
YvtheLion <37544171+YvtheLion@users.noreply.github.com>
2026-08-18 13:49:40 -0400

    Correction boucle de redirection infinie

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,15 +2,5 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.sthecroh.com' }],
-        destination: 'https://sthecroh.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
 };
 module.exports = nextConfig;
